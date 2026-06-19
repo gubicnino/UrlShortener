682fee2e48aa0a4354dd5ebe1bafd4adb1a1a1cc
Nino <gubicnino@gmail.com>
2026-06-19 21:11:40 +0200

    Initial setup

---
+++ src/main/java/si/solve_x/naloga/main/MainApplication.java
@@ -0,0 +1,13 @@
+package si.solve_x.naloga.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/si/solve_x/naloga/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package si.solve_x.naloga.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
